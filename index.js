f8ea81334be84f44cea40c8912ab208e7e3cd181
GavinSreesangkom <sreesg@uw.edu>
2019-04-13 17:27:15 -0700

    added index posts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let express = require('express');
 const bodyParser = require('body-parser');
+var router = express.Router();
 let app = express();
 app.use(bodyParser.json())
 
@@ -7,10 +8,42 @@ let PORT = 8080;
 
 // app.get("");
 
-app.post("/like", (req, res) => {
-    console.log(req.body)
-    res.send("Done");
-});
+app.route('/like')
+    .get(function (req, res) {
+    res.send('Number of likes')
+    })
+    .post(function (req, res) {
+    res.send('Like EYYYYY')
+    })
+
+app.route('/UserID')
+    .get(function (req, res) {
+    res.send('Get a User ID')
+    })
+    .post(function (req, res) {
+    res.send('Post User ID')
+    })
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`))
+
+app.route('/PostID')
+    .get(function (req, res) {
+    res.send('Get a Post ID')
+    })
+    .post(function (req, res) {
+    res.send('Post Post ID')
+    })
+
+app.route('/Balance')
+    .get(function (req, res) {
+    res.send('Get current balance')
+    })
+    .post(function (req, res) {
+    res.send('Add balance')
+    })
+
+
+
+
+
 
