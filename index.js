eec7f810fa23a4c0cd681d190497cb45d02efd63
GavinSree <sreesg@uw.edu>
2019-04-13 17:45:14 -0700

    added functions

--- index.js
+++ index.js
@@ -11,37 +11,43 @@ let PORT = 8080;
 app.route('/like')
     .get(function (req, res) {
     res.send('Number of likes')
+    //req.query.numLikes
     })
     .post(function (req, res) {
     res.send('Like EYYYYY')
+    makeLike(req.body.userid, req.body.postid)
     })
 
-app.route('/UserID')
+app.route('/userid')
     .get(function (req, res) {
     res.send('Get a User ID')
-    })
-    .post(function (req, res) {
-    res.send('Post User ID')
     })
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`))
 
-app.route('/PostID')
+app.route('/postid')
     .get(function (req, res) {
     res.send('Get a Post ID')
     })
     .post(function (req, res) {
     res.send('Post Post ID')
+    makePost(req.body.photo, req.body.userid, req.body.desc)
     })
 
-app.route('/Balance')
+app.route('/balance')
     .get(function (req, res) {
     res.send('Get current balance')
     })
     .post(function (req, res) {
     res.send('Add balance')
+    makeBalance(req.body.amount, req.body.userid)
     })
 
+app.route('/withdraw')
+    .post(function (req, res) {
+    res.send('Withdraw monet')
+    makeWithdraw(req.body.userid, req.body.amount)
+    })
 
 
 
