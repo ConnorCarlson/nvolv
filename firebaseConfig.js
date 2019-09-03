3ef808ae7862b5d1f7dc80ac8e49139078278b8a
ConnorCarlson <40050816+ConnorCarlson@users.noreply.github.com>
2019-09-02 21:02:53 -0700

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-exports.configInfo = {
-    apiKey: "AIzaSyDZP1cg1LdqRyfPo169XkxxKwAnSs9v51A",
-    authDomain: "nvolv-project.firebaseapp.com",
-    databaseURL: "https://nvolv-project.firebaseio.com",
-    projectId: "nvolv-project",
-    storageBucket: "nvolv-project.appspot.com",
-    messagingSenderId: "581064234562"
-}
